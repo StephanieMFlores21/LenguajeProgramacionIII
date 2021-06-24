3a9eff070d380680dcf3d0f083257c068b255be6
StephanieMFlores21 <floresmariela698@gmail.com>
2021-06-23 23:10:09 -0600

    Primer Commit
    Primer ejercicio del segundo parcial, Arreglos, Seccion 1201

---
+++ LenguajeProgramacionIII/FrmArreglos.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace LenguajeProgramacionIII
+{
+    partial class FrmArreglos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ArregloslistBox = new System.Windows.Forms.ListBox();
+            this.ejecutarbutton = new System.Windows.Forms.Button();
+            this.PaiseslistBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // ArregloslistBox
+            // 
+            this.ArregloslistBox.FormattingEnabled = true;
+            this.ArregloslistBox.Location = new System.Drawing.Point(30, 134);
+            this.ArregloslistBox.Name = "ArregloslistBox";
+            this.ArregloslistBox.Size = new System.Drawing.Size(124, 121);
+            this.ArregloslistBox.TabIndex = 0;
+            // 
+            // ejecutarbutton
+            // 
+            this.ejecutarbutton.Location = new System.Drawing.Point(218, 57);
+            this.ejecutarbutton.Name = "ejecutarbutton";
+            this.ejecutarbutton.Size = new System.Drawing.Size(104, 39);
+            this.ejecutarbutton.TabIndex = 1;
+            this.ejecutarbutton.Text = "Ejecutar";
+            this.ejecutarbutton.UseVisualStyleBackColor = true;
+            this.ejecutarbutton.Click += new System.EventHandler(this.ejecutarbutton_Click);
+            // 
+            // PaiseslistBox
+            // 
+            this.PaiseslistBox.FormattingEnabled = true;
+            this.PaiseslistBox.Location = new System.Drawing.Point(218, 134);
+            this.PaiseslistBox.Name = "PaiseslistBox";
+            this.PaiseslistBox.Size = new System.Drawing.Size(132, 147);
+            this.PaiseslistBox.TabIndex = 2;
+            // 
+            // FrmArreglos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 463);
+            this.Controls.Add(this.PaiseslistBox);
+            this.Controls.Add(this.ejecutarbutton);
+            this.Controls.Add(this.ArregloslistBox);
+            this.Name = "FrmArreglos";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ArregloslistBox;
+        private System.Windows.Forms.Button ejecutarbutton;
+        private System.Windows.Forms.ListBox PaiseslistBox;
+    }
+}
+
---
+++ LenguajeProgramacionIII/FrmArreglos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LenguajeProgramacionIII
+{
+    public partial class FrmArreglos : Form
+    {
+        public FrmArreglos()
+        {
+            InitializeComponent();
+        }
+
+        private void ejecutarbutton_Click(object sender, EventArgs e)
+        {
+            //FORMA N° 1 DE INICIALIZAR
+            int[] edades = new int[5];
+
+            edades[0] = 10;
+            edades[1] = 20;
+            edades[2] = 30;
+            edades[3] = 40;
+            edades[4] = 50;
+
+            //FORMA N° 2
+            string[] paises = new string[5] {"Honduras", "Costa Rica", "Guatemala", "El Salvador", "Nicaragua"};
+
+            for (int i = 0; i < edades.Length; i++)
+            {
+                ArregloslistBox.Items.Add(edades[i]);
+            }
+
+            foreach (var item in paises)
+            {
+                PaiseslistBox.Items.Add(item);
+            }
+        }
+    }
+}
